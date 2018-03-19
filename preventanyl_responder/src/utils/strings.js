877a3b0dc8b314a128264a52c2853e7c71702333
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-03-18 23:28:24 -0700

    Added multi-line capabilites, new functions to strings under utils, description is now multiline

--- preventanyl_responder/src/utils/strings.js
+++ preventanyl_responder/src/utils/strings.js
@@ -5,3 +5,37 @@ export const replaceAllRegex = (target, search, replacement) => {
 export const replaceAll = (target, search, replacement) => {
     return target.split(search).join(replacement);
 }
+
+export const wordWrap = (str, maxWidth) => {
+    newLineStr = "\n"; 
+    done = false; 
+    res = '';
+    
+    while (!done) {              
+        found = false;
+        // Inserts new line at first whitespace of the line
+        for (i = maxWidth - 1; i >= 0; --i)
+            if (whiteSpaceFind (str.charAt (i))) {
+                res = res + [str.slice (0, i), newLineStr].join ('');
+                str = str.slice (i + 1);
+                found = true;
+                break;
+            }
+
+        // Inserts new line at maxWidth position, the word is too long to wrap
+        if (!found) {
+            res += [str.slice(0, maxWidth), newLineStr].join('');
+            str = str.slice(maxWidth);
+        }
+
+        if (str.length < maxWidth)
+            done = true;
+    }
+
+    return res + str;
+}
+
+export const whiteSpaceFind = (x) => {
+    var white = new RegExp(/^\s$/);
+    return white.test(x.charAt(0));
+};
