7a2456977c5e297fd6691faa8201b22be6111a24
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-04-08 13:33:42 -0700

    Adding responding component, abstracted web page opening to become a sub component

---
+++ preventanyl_responder/src/utils/Links.js
@@ -0,0 +1,10 @@
+export default class Links {
+
+    static urls = Object.freeze (
+        {
+            "PAYPAL_ME"         : "https://www.paypal.me/preventanylApp",
+            "OVERDOSE_RESPONSE" : "https://www2.gov.bc.ca/gov/content/overdose/what-you-need-to-know/responding-to-an-overdose"
+        }
+    );
+
+}
